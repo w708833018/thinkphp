97f7d1a08d94031eed34b6d933f0b55e1ddfff28
wl <708833018@qq.com>
2013-10-22 18:23:54 +0800

    phpsso

---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'phpcms',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => 'test_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
