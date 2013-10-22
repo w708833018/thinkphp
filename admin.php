87ff8ffdb64791e9238f3ade83de8f012a68e060
wls <708833018@qq.com>
2013-10-22 19:01:10 +0800

    edit

--- admin.php
+++ admin.php
@@ -1,8 +1,8 @@
 <?php
 
-		define('APP_NAME','Admin');
-		define('APP_PATH','./Admin/');
-		require ('./ThinkPHP/ThinkPHP.php');
+	define('APP_NAME','Admin');
+	define('APP_PATH','./Admin/');
+	require ('./ThinkPHP/ThinkPHP.php');
 
 
 
