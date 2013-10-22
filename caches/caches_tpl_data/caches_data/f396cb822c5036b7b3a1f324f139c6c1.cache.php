e4eca820c77bdd6bcd0c0f818dde5166f73d1165
wl <708833018@qq.com>
2013-10-22 18:20:38 +0800

    caches

---
+++ caches/caches_tpl_data/caches_data/f396cb822c5036b7b3a1f324f139c6c1.cache.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  1 => 
+  array (
+    'id' => '1',
+    'catid' => '9',
+    'typeid' => '0',
+    'title' => '測試一下',
+    'style' => '',
+    'thumb' => '',
+    'keywords' => '測 試 一測試一下下',
+    'description' => '測試一下測試一下測試一下',
+    'posids' => '0',
+    'url' => 'http://localhost/index.php?m=content&c=index&a=show&catid=9&id=1',
+    'listorder' => '0',
+    'status' => '99',
+    'sysadd' => '1',
+    'islink' => '0',
+    'username' => 'admin',
+    'inputtime' => '1375883914',
+    'updatetime' => '1376309065',
+    'hitsid' => 'c-1-1',
+    'views' => '5',
+    'yesterdayviews' => '2',
+    'dayviews' => '2',
+    'weekviews' => '2',
+    'monthviews' => '5',
+  ),
+);
+?>
