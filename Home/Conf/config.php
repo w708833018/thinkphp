d89327ac0c893d0fbecef324333b15f3df40dd50
wlcopy <708833018@qq.com>
2013-10-25 16:31:05 +0800

    输出和模型使用1

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -1,5 +1,16 @@
 <?php
+
 return array(
-	//'配置项'=>'配置值'
+	'URL_PATHINFO_DEPR' => '/' ,        //修改url的分隔符
+	'TMPL_L_DELIM'      => '{',         //左定界符
+	'TMPL_R_DELIM'      => '}',         //右定界符
+	'DB_TYPE'           => 'mysql',     //数据库类型
+	'DB_HOST'           => 'localhost', //主机
+	'DB_NAME'           => 'wolf',      //数据库名
+	'DB_USER'           => 'root',      //用户名
+	'DB_PWD'            => 'wanglei',   //密码
+	'DB_PORT'           => '3306',      //端口
+	'DB_PREFIX'         => 'WL_',       //表前缀
+	'SHOW_PAGE_TRACE'   =>  true,       //错误追踪
 );
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 		define('APP_NAME','Home');
 		define('APP_PATH','./Home/');
+		define('APP_DEBUG',True);
 		require ('./ThinkPHP/ThinkPHP.php');
 
 
