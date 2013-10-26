ab5a52bcbc3480dc74b7093266bf2dd24a2d06e3
wls <708833018@qq.com>
2013-10-26 21:51:22 +0800

    增删改查

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1,13 +1,52 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
+
 class IndexAction extends Action {
     public function index(){
 	    $head_title = '测试';
-	   // $do = new Model('Member');
+	    $action = $_GET['action'] ? $_GET['action'] : $_POST['action'];
+	    $post = $_POST['post'] ;
 	    $do = M('Member');//实例化模型的简单方式
-	    $arr = $do->select();
+	    switch ($action)
+	    {
+		    case 'add':
+				    if($post['username']){
+					    $do->username=$post['username'];
+					    $do->age=$post['age'];
+					    if($do->add()){
+						    header('Location:?');
+					    }
+				    }
+				break;
+		    case 'edit':
+			    $userid = $_GET['userid'];
+			    if($userid){
+				    $item = $do->find($userid);
+				    $this->assign('item',$item);
+			    }
+				    if($post['username']){
+					   $count =  $do->save($post);
+							if($count || $count==0 ){
+								header('Location:?');
+							}else{
+							echo '更新失败！';
+							};
+			           }
+			    break;
+
+		    case 'delete':
+			    $userid = $_GET['userid'];
+			    if($userid){
+				 $do->delete($userid);
+				    header('Location:?');
+			    }
+			    break;
+		    default:
+			    $do->where('userid =21')->getField('username');//获取指定字段数据 ；
+			    $arr = $do->select();
+			    $this->assign('arr',$arr);
+	    }
 	    $this->assign('head_title',$head_title);
-	    $this->assign('name',$arr);
+	    $this->assign('action',$action);
 	    $this->display();
     }
 }
