b49a937913c11a25d9916073cd6b6025d713c0e0
wls <708833018@qq.com>
2013-10-27 20:44:03 +0800

    增删改查

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -1,52 +1,65 @@
 <?php
 
 class IndexAction extends Action {
-    public function index(){
-	    $head_title = '测试';
-	    $action = $_GET['action'] ? $_GET['action'] : $_POST['action'];
-	    $post = $_POST['post'] ;
-	    $do = M('Member');//实例化模型的简单方式
-	    switch ($action)
-	    {
-		    case 'add':
-				    if($post['username']){
-					    $do->username=$post['username'];
-					    $do->age=$post['age'];
-					    if($do->add()){
-						    header('Location:?');
-					    }
-				    }
-				break;
-		    case 'edit':
-			    $userid = $_GET['userid'];
-			    if($userid){
-				    $item = $do->find($userid);
-				    $this->assign('item',$item);
-			    }
-				    if($post['username']){
-					   $count =  $do->save($post);
-							if($count || $count==0 ){
-								header('Location:?');
-							}else{
-							echo '更新失败！';
-							};
-			           }
-			    break;
 
-		    case 'delete':
-			    $userid = $_GET['userid'];
-			    if($userid){
-				 $do->delete($userid);
-				    header('Location:?');
-			    }
-			    break;
-		    default:
-			    $do->where('userid =21')->getField('username');//获取指定字段数据 ；
-			    $arr = $do->select();
-			    $this->assign('arr',$arr);
-	    }
-	    $this->assign('head_title',$head_title);
-	    $this->assign('action',$action);
-	    $this->display();
+			private  $head_title;
+
+			private  $post;
+
+			private  $userid;
+
+			private  $do;
+
+		function __construct(){
+			parent::__construct();
+			$this->do       = M('Member');
+			$this->post    = $_POST['post'] ;
+			$this->head_title    = '测试' ;
+			$this->userid = $_GET['userid'];
+			$this->assign('head_title',$this->head_title);
+		}
+
+
+    public function index(){
+	    $do = M('Member');//实例化模型的简单方式
+		 $arr = $do->select();
+		$this->assign('arr',$arr);
+	      $this->display();
     }
+
+	public function edit(){
+		if($this->userid){
+			$item = $this->do->find($this->userid);
+			$this->assign('item',$item);
+		}
+		if($this->post['username']){
+			$count =  $this->do->save($this->post);
+			if($count >= 0 ){
+				header('Location:'.__URL__);
+			}else{
+				$this->error('更新失败');
+			};
+		}
+		$this->assign('action','edit');
+		$this->display('index');
+	}
+	public  function add(){
+		if($this->post['username']){
+			if($this->do->add($this->post)){
+				header('Location:'.__URL__);
+			}else{
+				$this->error('添加失败');
+			}
+		}
+		$this->assign('action','add');
+		$this->display('index');
+	}
+
+	public function  del(){
+		if($this->userid){
+			$this->do->delete($this->userid);
+			header('Location:'.__URL__);
+		}
+	}
+
 }
