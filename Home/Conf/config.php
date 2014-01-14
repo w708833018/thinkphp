2fcd34aea7f110522433c959d300657858999e0c
wlcopy <708833018@qq.com>
2014-01-14 10:39:41 +0800

    表单验证

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -21,6 +21,7 @@ return array(
 	'TMPL_PARSE_STRING' =>  array(
 			'__CSS__' => __ROOT__.'/Public/CSS',
 			'__JS__'    => __ROOT__. '/Public/JS',
+			'__IMG__'    => __ROOT__. '/Public/imgs',
 	),
 	'URL_MODEL'                => 2,
 );
