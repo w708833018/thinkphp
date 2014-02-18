3e98d1aea83fc8816b77fa5bcd0c01f38e78a957
wls <708833018@qq.com>
2014-02-18 21:47:17 +0800

    后台

---
+++ Modules/Admin/Action/IndexAction.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class IndexAction extends HomeAction {
+
+		public  function index(){
+			$this->display();
+		}
+
+}
---
+++ Modules/Admin/Conf/config.php
@@ -0,0 +1,13 @@
+
+<?php
+ return array(
+		'TMPL_PARSE_STRING' =>  array(
+				'__Public__' => C('APP_GROUP_PATH').'/Admin/Public',
+				'__CSS__' =>  C('APP_GROUP_PATH').'/Admin/Public/CSS',
+				'__JS__'    =>  C('APP_GROUP_PATH').'/Admin/Public/JS',
+				'__IMG__'    => C('APP_GROUP_PATH').'/Admin/Public/Images',
+		),
+		'URL_MODEL'                => 1,
+		'URL_HTML_SUFFIX'          => '',
+ );
+?>
