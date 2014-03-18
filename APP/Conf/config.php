5dc849583b6e46de4d54fb73ba684fa75ca72c7e
wlcopy <708833018@qq.com>
2014-03-18 17:33:57 +0800

    图片上传

--- APP/Conf/config.php
+++ APP/Conf/config.php
@@ -10,10 +10,12 @@
 		'DEFAULT_GROUP'		     =>'Home',       //默认分组
 		'APP_GROUP_MODE'         =>1,             //开启独立分组 0：普通分组 1：独立分组
 		'APP_GROUP_PATH'         =>'../Modules' ,     //独立分组目录
-		'URL'                    => 'http://'.$_SERVER['HTTP_HOST']     //独立分组目录
+		'URL'                    => 'http://'.$_SERVER['HTTP_HOST'],     //独立分组目录
 		//'TMPL_FILE_DEPR'       =>  '_',      //模板层次
 	    //DEFAULT_THEME'         =>  'index',   //默认模板主题
 	    //'TMPL_DETECT_THEME'    =>   true,       //自动侦测模板主题
-		//'THEME_LIST'           =>  'yellow,red,index',        //自动侦测模板主题
+		//'THEME_LIST'           =>  'yellow,red,index',           //自动侦测模板主题
+		 'UPLOAD_FILE'           =>  './Uploads/',                  //上传文件保存路径
+		 'UPLOAD_URL'            =>  'http://'.$_SERVER['HTTP_HOST'].'/Uploads/',          //上传文件访问路径
 	);
 ?>
--- APP/Lib/Action/BaseAction.class.php
+++ APP/Lib/Action/BaseAction.class.php
@@ -28,4 +28,34 @@ class BaseAction  extends  Action{
 	}
 
 
+	/**
+	 * 图片上传
+	 */
+	public function imgUpload(){
+		import('ORG.Net.UploadFile');
+		$config =   array(
+			'allowExts'         =>  array('jpg','jpeg','gif','png'),    // 允许上传的文件后缀 留空不作后缀检查
+			'autoSub'           =>  true,// 启用子目录保存文件
+			'subType'           =>  'custom',// 子目录创建方式 可以使用hash date custom
+			'subDir'            =>  'images/'.$_GET['cate_name'].'/'.date('Ymd',time()).'/', // 子目录名称 subType为custom方式后有效
+			'savePath'          =>   C('UPLOAD_FILE'),// 上传文件保存路径
+		);
+		$do = new UploadFile($config);
+		if($do->upload()){
+			$info = $do->getUploadFileInfo();
+			$this->ajaxReturn(array(
+				'url'       =>$info[0]['savename'],    //保存后的文件路径
+				'title'     =>htmlspecialchars($_POST['pictitle'], ENT_QUOTES), //文件描述，对图片来说在前端会添加到title属性上
+				'original'  =>$info[0]['name'],        //原始文件名
+				'state'     =>'SUCCESS',               //上传状态，成功时返回SUCCESS,其他任何值将原样返回至图片上传框中
+			));
+		}else{
+			$this->ajaxReturn(array(
+				'state'     =>$do->getErrorMsg(),
+			));
+		}
+
+	}
+
+
 }
