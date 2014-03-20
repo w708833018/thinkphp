5d9a5f5120418a16451b24ad8f66daae7154cf0f
wlcopy <708833018@qq.com>
2014-03-20 17:38:41 +0800

    文章管理

--- APP/Lib/Model/ArticleRelationModel.class.php
+++ APP/Lib/Model/ArticleRelationModel.class.php
@@ -11,17 +11,67 @@ class ArticleRelationModel  extends  RelationModel{
 	protected $_link = array(
 		'position' => array(
 			'mapping_type' =>MANY_TO_MANY,
-			'mapping_name' =>'position',
 			'foreign_key' =>'article_id',
 			'relation_foreign_key' =>'position_id',
-			'relation_table'=>'omgt_article_position'
+			'relation_table'=>'omgt_article_position',
+			'mapping_fields'  =>'id',
+		),
+		'category' => array(
+			 'mapping_type' =>BELONGS_TO,
+			 'foreign_key'  =>'catid',
+			 'mapping_fields'  =>'name',
+			 'as_fields'   =>'name:catename',
+
 		)
 	);
 
 	protected $_auto = array(
 			array('status',3),
 			array('addtime','time',1,'function'),
-			array('edittime','time',1,'function'),
+			array('edittime','time',3,'function'),
 	);
+
+	public function getList($status = 3){
+		return $this->field('content',true)->relation(true)->where(array('status'=>$status))->select();
+	}
+
+	/**
+	 * 批量还原
+	 * @param $id
+	 */
+	public function restore($id){
+		if(is_array($id)){
+			foreach($id as $v){
+				$this->restore($v);
+			}
+		}else{
+			$this->where(array('id'=>$id))->setField(array('status'=>3));
+		}
+
+	}
+
+	/**
+	 * 批量删除
+	 * @param        $id
+	 * @param string $status
+	 */
+	public function del($id,$status=''){
+		if(is_array($id)){
+			foreach($id as $v){
+				$this->del($v,$status);
+			}
+		}else{
+			switch($status){
+				case 3:
+					$this->where(array('id'=>$id))->setField(array('status'=>1));
+					break;
+				case 1:
+					$this->where(array('id'=>$id))->delete();
+					M('article_position')->where(array('article_id'=>$id))->delete();
+					break;
+			}
+		}
+	}
+
 
 }
--- Modules/Admin/Action/ArticleAction.class.php
+++ Modules/Admin/Action/ArticleAction.class.php
@@ -18,11 +18,14 @@ class ArticleAction extends AdminAction {
 		$menu = array(
 			'add' => '添加文章',
 			'index' => '文章列表',
+			'recyle' => '回收站',
 		);
 		return  $menu;
 	}
 
 	public function index(){
+		$this->list   = D('ArticleRelation')->getList();
+		$this->status = 3;
 		$this->display();
 	}
 
@@ -36,7 +39,7 @@ class ArticleAction extends AdminAction {
 					}
 				}
 			}
-			$this->ajaxReturn(array('success'=>1,'message'=>'添加成功','referer'=>U('Node/index')));
+			$this->ajaxReturn(array('success'=>1,'message'=>'添加成功','referer'=>U(MODULE_NAME.'/index')));
 		}else{
 			$list = M('category')->field('id,name,parent_id')->order('sort')->where(array('cate'=>1))->select();
 			$tree = new Tree();
@@ -52,26 +55,67 @@ class ArticleAction extends AdminAction {
 
 	public function edit(){
 		if(I('post.')){
-			$data = I('post.');
-			$levPid = explode('_',$data['levPid']);
-			$data['level'] = $levPid[0]+1;
-			$data['pid']   = $levPid[1];
-			M('node')->save($data);
-			$this->ajaxReturn(array('success'=>1,'message'=>'修改成功','referer'=>U('Node/index')));
+			D('ArticleRelation')->create();
+			if(D('ArticleRelation')->save()){
+				if(isset($_POST['position']) && !empty($_POST['position'])){
+					M('article_position')->where(array('article_id'=>I('id')))->delete();
+					foreach($_POST['position'] as $v){
+						M('article_position')->add(array('article_id'=>I('id'),'position_id'=>$v));
+					}
+				}else{
+					M('article_position')->where(array('article_id'=>I('id')))->delete();
+				}
+			}
+			$this->ajaxReturn(array('success'=>1,'message'=>'修改成功','referer'=>U(MODULE_NAME.'/'.I('action'))));
 		}else{
-			$nodeList = M('node')->where('level <= 2')->field('title,id,pid,level')->select();
-			$this->nodeList = node_merge($nodeList);
-			$this->item = M('node')->find(I('id'));
-			$this->menu = array_merge($this->menu,array('edit'=>'修改节点'));
+			$item = D('ArticleRelation')->relation('position')->find(I('id'));
+			$list = M('category')->field('id,name,parent_id')->order('sort')->where(array('cate'=>1))->select();
+			$list = Category::init($list);
+			$tree = new Tree();
+			$tree->icon = array('│', '├─ ', '└─ ');
+			$tree->init($list);
+			$str = "<option value=\$id \$selected>\$spacer\$name</option>";
+			$this->cate = $tree->get_tree(6,$str,$item['catid']);
+			$this->menu = array_merge(self::menu(),array('edit'=>'修改文章'));
+			$pids = array();
+			foreach($item['position'] as $v){
+				$pids[] = $v['id'];
+			}
+			$this->assign('pids',$pids);
+			$this->assign('action',I('action'));
+			$this->assign('item',$item);
+			$this->position = M('position')->where(array('catid'=>array(array('eq',6),array('eq',0), 'or')))->select();
 			$this->display('add');
 		}
 
 	}
 
+	/**
+	 * 回收站
+	 */
+	public function recyle(){
+		$this->list   = D('ArticleRelation')->getList(1);
+		$this->status = 1;
+		$this->display('index');
+	}
+
+	/**
+	 * 还原
+	 */
+	public function restore(){
+		I('id') or $this->error('请选择文章');
+		D('ArticleRelation')->restore($_POST['id']);
+		$this->success('还原成功');
+	}
+
+	/**
+	 * 删除
+	 */
 	public function delete(){
-		I('id') or $this->error('请选择节点');
-		M('node')->where(array('id|pid'=>I('id')))->delete();
-		$this->redirect('index');
+		I('id') or $this->error('请选择文章');
+		$id = I('post.') ? $_POST['id'] : I('id');
+	    D('ArticleRelation')->del($id,I('status'));
+		$this->success('操作成功');
 	}
 
 
