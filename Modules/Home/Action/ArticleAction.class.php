654fdc02e34110aa5095da3eb8050fec048c06af
wlcopy <708833018@qq.com>
2014-03-28 16:18:54 +0800

    show

--- Modules/Home/Action/ArticleAction.class.php
+++ Modules/Home/Action/ArticleAction.class.php
@@ -8,11 +8,13 @@ class ArticleAction extends HomeAction {
 
 	public function index(){
 		$where = I('catid') ? array('catid'=>I('catid')) : '';
-		$this->list = M('article')->field('title,addtime,introduce')->order('edittime desc')->where($where)->select();
+		$this->list = M('article')->field('id,title,addtime,introduce')->order('edittime desc')->where($where)->select();
 		$this->display('Article/index');
 	}
 
 	public function show(){
+		I('id') or _404('页面不存在！');
+		$this->item = M('article')->find(I('id'));
 		$this->display();
 	}
 } 
--- Modules/Home/Action/IndexAction.class.php
+++ Modules/Home/Action/IndexAction.class.php
@@ -8,7 +8,7 @@ class IndexAction extends HomeAction {
 	}
 
 	public function index(){
-		$this->display('Public::common');
+		$this->display();
 	}
 
 }
